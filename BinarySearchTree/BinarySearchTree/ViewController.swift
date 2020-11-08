8cd036bff1f5ff942cf13ee5e0d8135e6e7893e6
sunzhichao <sunzhichao@minijoy.work>
2020-11-08 22:17:32 +0800

    init binary search tree

---
+++ BinarySearchTree/BinarySearchTree/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BinarySearchTree
+//
+//  Created by SuniMac on 2020/11/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
